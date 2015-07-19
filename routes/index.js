4ca85d51003cafb4a5f003c100d992598e70eb8f
Vitaliy Krasylovets <Vitaliy@Vitaliys-MacBook-Pro.local>
2015-07-19 02:24:45 -0400

    Added Brain tree payment

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
+var braintree = require("braintree");
+
+var gateway = braintree.connect({
+  environment: braintree.Environment.Sandbox,
+  merchantId: "kk9kgdz22f4hcdd5",
+  publicKey: "mkt74f8zbdprkn6k",
+  privateKey: "f094b7cba797f3d0da836c894d46aba5"
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send("hello Worlds");
+  res.json("{\"HELLO WORLD\"}");
+});
+
+/**
+ * Route that returns a token to be used on the client side to tokenize payment details
+ */
+router.get('/client_token', function(req, res, next) {
+	gateway.clientToken.generate({}, function (err, response) {
+    	if (err) { 
+    		throw err;
+    	}
+
+    	res.send(response.clientToken);
+  		console.log('token generated: ' + response.clientToken);
+  	});
+});
+
+/**
+ * Route to process a sale transaction
+ */
+router.post('/nonce/transaction', function(req, res, next) {
+	var transaction = request.body;
+	var amountCharged = transaction.amount;
+    
+    gateway.transaction.sale({
+    	amount: amountCharged,
+    	paymentMethodNonce: transaction.payment_method_nonce},
+    	function (err, result) {
+    	
+    	if (err)  {
+    		throw err;
+    	}
+
+    	console.log(util.inspect(result));
+    	res.json(result);
+  	});
 });
 
 module.exports = router;
